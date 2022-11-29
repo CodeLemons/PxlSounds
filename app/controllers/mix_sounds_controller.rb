16a573f40617780242799ed3347f034b8b71f2cf
Tomas Bui Lai <tomass123ss@gmail.com>
2022-11-29 14:01:46 +0000

    mix sound controller

--- app/controllers/mix_sounds_controller.rb
+++ app/controllers/mix_sounds_controller.rb
@@ -1,2 +1,37 @@
 class MixSoundsController < ApplicationController
+  # new, create, edit, update, destroy
+
+  def new
+    @mix_sound = Mix_sound.new
+  end
+
+  def create
+    @mix_sound = Mix_sound.new(mix_sound_params)
+    @mix_sound.save
+    redirect_to mix_sound_path(@mix_sound)
+  end
+
+  def edit
+    @mix_sound = Mix_sound.find(params[:id])
+  end
+
+  def update
+    @mix_sound.update(mix_sound_params)
+    redirect_to mix_sound_path(@mix_sound)
+  end
+
+  def destroy
+    @mix_sound = Mix_sound.find(params[:id])
+    @mix_sound.destroy
+
+    redirect_to mix_sound_path, status: :see_other
+  end
+
+  private
+
+  def mix_sound_params
+    params.require(:mix_sound).permit(:volume)
+  end
+
+
 end
