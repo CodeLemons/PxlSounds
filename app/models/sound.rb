d0c074bdd70e9d5e0f169ad021a35ce0f1fee419
Tomas Bui Lai <tomass123ss@gmail.com>
2022-11-29 12:32:31 +0000

    validation

--- app/models/sound.rb
+++ app/models/sound.rb
@@ -1,3 +1,13 @@
 class Sound < ApplicationRecord
   belongs_to :world
+
+  has_many :mix_sounds
+
+  validates :name, presence: true
+  validates :path, presence: true
+  validates :start_x, presence: true
+  validates :start_y, presence: true
+  validates :height, presence: true
+  validates :width, presence: true
+
 end
