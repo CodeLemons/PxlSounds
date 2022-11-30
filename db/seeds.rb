1d9f5b5296d48b98979e220c7ae807f19042497f
juan mendoza <j.mendoza712@gmail.com>
2022-11-30 11:58:35 +0000

    seeds partially done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+json file reading here!
+
+
+puts "Cleaning database"
+World.destroy_all
+puts "Database cleaned"
+
+
+puts "Creating a world"
+world = World.create(
+  name: "Castle",
+  description: "TODO",
+  image: "Castle.gif"
+)
+puts "#{world.name} created"
+
+puts "Creating sounds for #{world.name}"
+5.times do |index|
+  sound = Sound.create(
+    name: string
+    path: string name of file stored in assets
+    world_id: world.id
+    start_x: json file
+    start_y: json file
+    height: json file
+    width: json file
+  )
+  puts "#{sound.name} created"
+end
