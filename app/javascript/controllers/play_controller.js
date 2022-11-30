2bc0add56c446ad4d7dad833b705565dfa4c8570
Ali-Hamza Asam <asamalihamza@gmail.com>
2022-11-30 17:31:57 +0000

    Fixed stimulus partially working now

--- app/javascript/controllers/play_controller.js
+++ app/javascript/controllers/play_controller.js
@@ -1,17 +1,40 @@
 import { Controller } from "@hotwired/stimulus"
 
+import {Howl} from 'howler';
+
+
+
 // Connects to data-controller="play"
 export default class extends Controller {
-  static targets = ["rain", "guy"]
+  static targets = ["sound"]
   connect() {
     console.log("Hello from play area");
+  //   let sfx = new Howl({
+  //     src: []
+  // })
+
   }
 
-  rain() {
-    console.log("Hello from rain target");
+  toggleSound(e) {
+    console.log(e.target.dataset.soundFile);
+    this.#getSoundFiles();
+    let sfx = new Howl({
+      src: [e.target.dataset.soundFile],
+      loop: true,
+      onend: function() {
+        console.log("finished");
+      }
+    })
+    sfx.playing() ? sfx.stop() : sfx.play();
   }
 
-  guy() {
-    console.log("Hello from guy target");
+  #getSoundFiles() {
+    const array = [];
+    this.soundTargets.forEach((element) => {
+      array.push(element.dataset.soundFile);
+    });
+
+    console.log(array);
+    return array
   }
 }
