b8f10875707e4d7fa19b5bbb4af7825a06f3369a
lucas brown <lucas20brown@hotmail.co.uk>
2023-01-10 14:48:54 +0000

    fixed alis issues

--- app/controllers/worlds_controller.rb
+++ app/controllers/worlds_controller.rb
@@ -1,17 +1,16 @@
 class WorldsController < ApplicationController
-
   def show
     @world = World.find(params[:id])
   end
-  
+
   def index
     # @mix = Mix.find(params)
     @worlds = World.all
     @mixes = current_user.mixes
   end
-  
+
   def new
-    @world = World.new  
+    @world = World.new
   end
 
   def create
@@ -23,15 +22,14 @@ class WorldsController < ApplicationController
       render "new", status: :unprocessable_entity, notice: "FAILED"
     end
   end
-  
+
   def play
     @world = World.find(params[:world_id])
   end
 
-  private 
+  private
 
   def world_params
     params.require(:world).permit(:name, :description, :image)
   end
-
 end
